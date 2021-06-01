86517fe2431e6ec520ed5579a56ba208c912b083
Muhammad As'ad Muyassir <muhammad.asad@ui.ac.id>
2021-06-01 11:48:47 +0700

    Add Raspberry App

---
+++ Raspberry App/detector.py
@@ -0,0 +1,80 @@
+from tflite_runtime.interpreter import Interpreter
+import numpy as np
+
+class ObjectDetectorLite:
+    def __init__(self, model_path):
+        # Define lite graph and Load Tensorflow Lite model into memory
+        self.interpreter = Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+        
+        # Get input size from model
+        input_shape = self.input_details[0]['shape']
+        self.size = input_shape[:2] if len(input_shape) == 3 else input_shape[1:3]
+
+    def detect(self, image, threshold=0.1):
+        # Add a batch dimension
+        frame = np.expand_dims(image, axis=0)
+        
+        # run model
+        self.interpreter.set_tensor(self.input_details[0]['index'], frame)
+        self.interpreter.invoke()
+        
+        # get results
+        boxes = self.interpreter.get_tensor(self.output_details[0]['index'])
+        classes = self.interpreter.get_tensor(self.output_details[1]['index'])
+        scores = self.interpreter.get_tensor(self.output_details[2]['index'])
+        num = self.interpreter.get_tensor(self.output_details[3]['index'])
+        
+        # Find detected boxes coordinates
+        return self._boxes_coordinates(image,
+                                        np.squeeze(boxes[0]),
+                                        np.squeeze(classes[0]+1).astype(np.int32),
+                                        np.squeeze(scores[0]),
+                                        min_score_thresh=threshold)
+
+    def _boxes_coordinates(self,
+                        image,
+                        boxes,
+                        classes,
+                        scores,
+                        max_boxes_to_draw=20,
+                        min_score_thresh=.5):
+        """
+        This function groups boxes that correspond to the same location
+        and creates a display string for each detection 
+        
+        Args:
+          image: uint8 numpy array with shape (img_height, img_width, 3)
+          boxes: a numpy array of shape [N, 4]
+          classes: a numpy array of shape [N]
+          scores: a numpy array of shape [N] or None.  If scores=None, then
+            this function assumes that the boxes to be plotted are groundtruth
+            boxes and plot all boxes as black with no classes or scores.
+          max_boxes_to_draw: maximum number of boxes to visualize.  If None, draw
+            all boxes.
+          min_score_thresh: minimum score threshold for a box to be visualized
+        """
+        
+        if not max_boxes_to_draw:
+            max_boxes_to_draw = boxes.shape[0]
+        number_boxes = min(max_boxes_to_draw, boxes.shape[0])
+        detected_boxes = []
+        probabilities = []
+        categories = []
+        
+        for i in range(number_boxes):
+            if scores is None or scores[i] > min_score_thresh:
+                box = tuple(boxes[i].tolist())
+                detected_boxes.append(box)
+                probabilities.append(scores[i])
+                # KASIH IF BUAT CEK SEBERAPA GEDE UKURAN BOXNYA
+                # BUAT NENTUIN DIA TERMASUK LUBANG BESAR/SEDANG/KECIL
+                # TERUS APPEND KE CATEGORIES BUAT GANTIIN YANG DIBAWAH INI
+                # INI SEMENTARA DOANG, CUMA CETAK PERSENTASENYA
+                categories.append(self.category_index[classes[i]])
+        return np.array(detected_boxes), probabilities, categories
+
+    def get_input_size(self):
+        return self.size
---
+++ Raspberry App/main.py
@@ -0,0 +1,55 @@
+import numpy as np
+import picamera
+import argparse
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+
+from utils import draw_bounding_boxes_on_image_array
+from utils import load_image
+from detector import ObjectDetectorLite
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Object Detection')
+    parser.add_argument('--model_path', type=str, help='Specify the model path',
+                        default='model.tflite')
+    parser.add_argument('--confidence', type=float, help='Minimum required confidence level of bounding boxes',
+                        default=0.6)
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    detector = ObjectDetectorLite(model_path=args.model_path)
+    input_size = detector.get_input_size()
+    
+    plt.ion()
+    plt.tight_layout()
+    
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Object Detection')
+    fig.suptitle('Detecting')
+    #ada detecting, detected, sama pending
+    ax = plt.gca()
+    ax.set_axis_off()
+    tmp = np.zeros(input_size + [3], np.uint8)
+    preview = ax.imshow(tmp)
+    
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        while True:
+            stream = np.empty((480, 640, 3), dtype=np.uint8)
+            camera.capture(stream, 'rgb')
+            
+            image = load_image(stream)
+            boxes, scores, classes = detector.detect(image, args.confidence)
+            for label, score in zip(classes, scores):
+                print(label, score)
+            
+            if len(boxes) > 0:
+                draw_bounding_boxes_on_image_array(image, boxes, display_str_list=classes)
+            
+            preview.set_data(image)
+            fig.canvas.get_tk_widget().update()
