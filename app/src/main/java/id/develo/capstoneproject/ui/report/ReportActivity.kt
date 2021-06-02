2f557558084ab50b580431c6edbf7fa6e3efef8c
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2021-06-03 05:15:36 +0700

    Report Layout 1

---
+++ app/src/main/java/id/develo/capstoneproject/ui/report/ReportActivity.kt
@@ -0,0 +1,12 @@
+package id.develo.capstoneproject.ui.report
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.develo.capstoneproject.R
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
